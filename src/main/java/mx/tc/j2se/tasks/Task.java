a117a9d28e7f7d6e1dd1272b5482379b6a909239
Brenda Itzel <biih1996@hotmail.com>
2022-07-26 23:56:01 -0500

    P7 is almost ready! I couldn't test the incoming method on the platform because it threw me some errors when creating the "array" variable. The calendar method always gave me null and I don't know why.

--- src/main/java/mx/tc/j2se/tasks/Task.java
+++ src/main/java/mx/tc/j2se/tasks/Task.java
@@ -1,20 +1,36 @@
 package mx.tc.j2se.tasks;
 
+import java.time.LocalDateTime;
+
 public interface Task {
     String getTitle();
     void setTitle(String title);
     boolean isActive();
     void setActive(boolean active);
-    int getTime();
-    void setTime(int time);
-    int getStartTime();
-    int getEndTime();
-    int getRepeatInterval();
-    void setTime(int start, int end, int interval);
-    boolean isRepeated();
-    public void taskImpl();
-    public void taskImpl(String title, int time);
-    public void taskImpl(String title, int start, int end, int interval);
+    public LocalDateTime getTime();
+    void setTime(LocalDateTime time);
+    void setTime(int year, int month, int day, int hour, int minutes, int seconds);
 
-    int nextTimeAfter(int current);
+    LocalDateTime getStartTime();
+    LocalDateTime getEndTime();
+    long getRepeatInterval();
+
+    void setTime(int startYear, int startMonth, int startDay, int startHour, int startMinutes, int startSeconds, int endYear, int endMonth, int endDay, int endHour, int endMinutes, int endSeconds, long interval);
+
+    public boolean isRepeated();
+
+    void taskImpl();
+
+
+    //Constructor for a non-repetitive task
+    public void taskImpl(String title, int year, int month, int day, int hour, int minutes, int seconds);
+
+    void taskImpl(String title, int startYear, int startMonth, int startDay, int startHour, int startMinutes, int startSeconds, int endYear, int endMonth, int endDay, int endHour, int endMinutes, int endSeconds, long interval);
+
+    LocalDateTime nextTimeAfter(LocalDateTime current);
+    public LocalDateTime nextTimeAfter (int year, int month, int day, int hour, int minutes, int seconds);
+    boolean equals (Object o);
+    int hashCode();
+    String toString();
+
 }
